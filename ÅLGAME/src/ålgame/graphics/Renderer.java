c1f98009a185fe118017b10023654f78a113f99b
zakiisak <zakiisak@gmail.com>
2015-03-10 16:19:06 +0100

    Made a simple sample program

--- ÅLGAME/src/ålgame/graphics/Renderer.java
+++ ÅLGAME/src/ålgame/graphics/Renderer.java
@@ -2,7 +2,21 @@ package ålgame.graphics;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
+
 public class Renderer {
+	
+	private static UnicodeFont boundFont;
+	
+	public static void setFont(UnicodeFont font) {
+		boundFont = font;
+	}
+	
+	public static void drawString(String str, float x, float y) {
+		boundFont.drawString(x, y, str);
+	}
 	
 	public static void setColor(float r, float g, float b, float a) {
 		glColor4f(r, g, b, a);
@@ -22,6 +36,28 @@ public class Renderer {
 	
 	public static void drawQuad(float x, float y, float width, float height) {
 		glRectf(x, y, x + width, y + height);
+	}
+	
+	public static void setTextureEnabled(boolean enabled) {
+		if(enabled) glEnable(GL_TEXTURE_2D);
+		else glDisable(GL_TEXTURE_2D);
+	}
+	
+	public static void setBlendingEnabled(boolean enabled) {
+		if(enabled) glEnable(GL_BLEND);
+		else glDisable(GL_BLEND);
+	}
+	
+	public static void setBlendFunc(int sfactor, int dfactor) {
+		glBlendFunc(sfactor, dfactor);
+	}
+	
+	public static void setFontAntialisingEnabled(boolean enabled) {
+		if(enabled) {
+			setBlendingEnabled(true);
+			setBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		}
+		else setBlendingEnabled(false);
 	}
 	
 	public static void drawTexturedQuad(Image image, float x, float y, float width, float height) {
@@ -55,4 +91,32 @@ public class Renderer {
 		glEnd();
 	}
 	
+	@SuppressWarnings("unchecked")
+	public static UnicodeFont loadFont(String name, boolean bold, boolean italic, int size) {
+		UnicodeFont font = null;
+		if (name.toLowerCase().endsWith(".ttf")) {
+			try {
+				font = new UnicodeFont(name, size, bold, italic);
+				font.getEffects().add(new ColorEffect(java.awt.Color.white));
+				font.addAsciiGlyphs();
+				try {
+					font.loadGlyphs();
+				} catch (SlickException e) {
+					e.printStackTrace();
+				}
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
+		} else {
+			font = new UnicodeFont(new java.awt.Font(name, bold && italic ? 3 : bold ? 1 : italic ? 2 : 0, 24));
+			font.getEffects().add(new ColorEffect(java.awt.Color.white));
+			font.addAsciiGlyphs();
+			try {
+				font.loadGlyphs();
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
+		}
+		return font;
+	}
 }
--- ÅLGAME/src/ålgame/main/Main.java
+++ ÅLGAME/src/ålgame/main/Main.java
@@ -1,8 +1,15 @@
 package ålgame.main;
 
-import org.lwjgl.opengl.Display;
+import java.awt.Font;
 
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.newdawn.slick.UnicodeFont;
+
+import ålgame.geom.Entity;
 import ålgame.graphics.DisplayManager;
+import ålgame.graphics.Image;
 import ålgame.graphics.Renderer;
 
 public class Main {
@@ -12,10 +19,46 @@ public class Main {
 		
 		Renderer.setOrthographicProjection(0, Display.getWidth(), Display.getHeight(), 0, -1, 1); //For 2D rendering
 		
+		Renderer.setTextureEnabled(true);
+		Image testImage = new Image("white.png");
+		Entity e1 = new Entity(32, 32, 32, 32);
+		Entity e2 = new Entity(65, 64, 64, 64);
+		
+		Renderer.setFontAntialisingEnabled(true);
+		UnicodeFont font = Renderer.loadFont(Font.MONOSPACED, true, false, 24);
+		Renderer.setFont(font);
+		
+		float dt = 0.0f;
 		while(DisplayManager.isOpen()) {
+			long lt = System.nanoTime();
 			Renderer.clear();
-			Renderer.drawQuad(32, 32, 32, 32);
+			float[] color = {1, 1, 1};
+			if(e2.intersects(e1)) color = new float[] {1, 0, 0};
+			Renderer.setColor(color[0], color[1], color[2]);
+			Renderer.drawTexturedQuad(testImage, e1.getX(), e1.getY(), e1.getWidth(), e1.getHeight());
+			Renderer.drawTexturedQuad(testImage, e2.getX(), e2.getY(), e2.getWidth(), e2.getHeight());
+			
+			Renderer.setColor(1, 1, 1);
+			Renderer.drawString("Mouse X: " + Mouse.getX() + ", Mouse Y: " + Mouse.getY(), 128, 16);
+			
+			float ms = 2.0f;
+			if(Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+				e1.setY(e1.getY() - ms * dt);
+			}
+			if(Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+				e1.setY(e1.getY() + ms * dt);
+			}
+			if(Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+				e1.setX(e1.getX() - ms * dt);
+			}
+			if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+				e1.setX(e1.getX() + ms * dt);
+			}
+			
 			DisplayManager.update();
+			float dfps = 1000000000.0f / (float) (System.nanoTime() - lt);
+			dt = 1.0f;
+			Display.setTitle("fps: " + dfps);
 		}
 		
 		DisplayManager.destroy();
